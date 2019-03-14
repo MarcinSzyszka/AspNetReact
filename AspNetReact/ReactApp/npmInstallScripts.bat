67fd22985545c1e5013948fa034e11d6115da1f0
Marcin Szyszka <marcin.szyszka@action.pl>
2019-03-14 09:01:19 +0100

    Added batch file with npm scripts to install tool for react

---
+++ AspNetReact/ReactApp/npmInstallScripts.bat
@@ -0,0 +1,4 @@
+npm install --save-dev webpack
+npm i -g webpack-cli
+npm init
+npm install babel-core babel-loader@7 babel-preset-env babel-preset-react babel-preset-stage-2 react react-dom --save-dev
